75824fb4eef782b6e00e28536743358e68caad26
Vanessa McHale <tmchale@wisc.edu>
2017-05-26 17:42:16 -0500

    comment

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -4,6 +4,7 @@ import Criterion.Main
 import Brainheck
 import qualified Data.Text.IO as TIO
 
+-- FIXME use nfIO for final result + use env correctly
 main = TIO.readFile "bf/helloworld.bf" >>= \file -> defaultMain [ 
     bgroup "parseBrainheck" [ 
         bench "helloworld.bf" $ whnf (parseBrainheck "bf/helloworld.bf") file ] 
